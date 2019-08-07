5999ebc870b0b1f2a9a6ef8a9aa187b98fffe56a
blaineanthonywestern <blaine.a.western@gmail.com>
2019-08-07 17:40:31 +1200

    Add templates

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,18 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="nav-container">
+    <div className="nav-items">
+    <div ><Link to="/films" className="link" activeStyle={{ color: "black" , backgroundColor: "whitesmoke"}} >FILMS</Link></div>
+    <div className="nav-spacer"></div>
+    <div ><Link to="/about" className="link" activeStyle={{ color: "black" , backgroundColor: "whitesmoke"}} >ABOUT</Link></div>
+    <div className="nav-spacer"></div>
+    <div ><Link to="/contact" className="link" activeStyle={{ color: "black" , backgroundColor: "whitesmoke"}} >CONTACT</Link></div>
     </div>
+    <div className="logo">LOUIS-JACK</div>
   </header>
 )
 
@@ -40,3 +25,6 @@ Header.defaultProps = {
 }
 
 export default Header
+
+
+// activeClassName="active-nav-link"
---
+++ src/pages/films.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function films() {
+    return (
+    <Layout>
+        <SEO title="Films" />    
+    </Layout>
+    )
+}
