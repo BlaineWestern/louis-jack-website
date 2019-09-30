1d9933838aa9ca83e34728a11837bf547f575425
blaineanthonywestern <blaine.a.western@gmail.com>
2019-09-30 16:27:06 +1300

    tweak styling for responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,33 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./header.css"
 
+const activeStyle = {
+  color: "black",
+  backgroundColor: "whitesmoke",
+  textDecoration: "none",
+}
+
 const Header = ({ siteTitle }) => (
   <header className="nav-container">
     <div className="nav-items">
-    <div className="nav-spacer"></div>
-    <div ><Link to="/films" className="link" activeStyle={{ color: "black" , backgroundColor: "whitesmoke", textDecoration: "none"}} >FILMS</Link></div>
-    <div className="nav-spacer"></div>
-    <div ><Link to="/about" className="link" activeStyle={{ color: "black" , backgroundColor: "whitesmoke", textDecoration: "none"}} >ABOUT</Link></div>
-    <div className="nav-spacer"></div>
-    <div ><Link to="/contact" className="link" activeStyle={{ color: "black" , backgroundColor: "whitesmoke", textDecoration: "none"}} >CONTACT</Link></div>
+      <div className="nav-spacer"></div>
+      <div>
+        <Link to="/films" className="link" activeStyle={activeStyle}>
+          FILMS
+        </Link>
+      </div>
+      <div className="nav-spacer"></div>
+      <div>
+        <Link to="/about" className="link" activeStyle={activeStyle}>
+          ABOUT
+        </Link>
+      </div>
+      <div className="nav-spacer"></div>
+      <div>
+        <Link to="/contact" className="link" activeStyle={activeStyle}>
+          CONTACT
+        </Link>
+      </div>
     </div>
   </header>
 )
@@ -25,6 +43,3 @@ Header.defaultProps = {
 }
 
 export default Header
-
-
-// activeClassName="active-nav-link"
