9fae655b1e9d623a0d68840ad2de317f0c3ec770
blaineanthonywestern <blaine.a.western@gmail.com>
2019-10-06 08:19:27 +1300

    change player to disable controls

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactPlayer from "react-player"
+import Vimeo from "@u-wave/react-vimeo"
 import LandingTitle from "../components/LandingTitle"
 
 import Layout from "../components/layout"
@@ -9,14 +9,13 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="video-container">
-      <ReactPlayer
-        playing={true}
-        loop={true}
-        url={
-          "https://player.vimeo.com/video/360773446?api=1&background=1&autoplay=1&loop=1"
-        }
+      <Vimeo
+        className="video-player"
+        video="360773446"
+        autoplay
         width="100%"
-        height="100%"
+        loop="true"
+        controls="false"
       />
       <LandingTitle />
     </div>
