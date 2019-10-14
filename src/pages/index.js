739286c6791affcd2463ccd654962ae381afb2b5
blaineanthonywestern <blaine.a.western@gmail.com>
2019-10-14 21:56:21 +1300

    add buffer for video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Vimeo from "@u-wave/react-vimeo"
+import ReactTimeout from "react-timeout"
 import LandingTitle from "../components/LandingTitle"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="video-container">
-      <Vimeo
-        className="video-player"
-        video="365937618"
-        autoplay={true}
-        width="110%"
-        loop="true"
-        controls="false"
-      />
-      <LandingTitle />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const [play, setPlay] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setPlay(true)
+    }, 1000)
+  })
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="video-container">
+        {play && (
+          <Vimeo
+            className="video-player"
+            video="365937618"
+            autoplay={true}
+            width="110%"
+            loop="true"
+            controls="false"
+          />
+        )}
+        <LandingTitle />
+      </div>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export default ReactTimeout(IndexPage)
